779114c213263f68fbab62f752f9e95cad1aeca5
Ola <128837261+aleksandrawrombel@users.noreply.github.com>
2024-02-12 14:26:18 +0100

    header styling done, responsiveness added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ const Header = () => {
   return (
     <header className="bg-office_gray h-15 m-10 p-3 rounded-xl">
       <ul className="flex justify-between items-center">
-        <li className="border-black border-solid border-2 rounded-xl w-60 flex justify-center font-semibold hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out">
-          <a href="#" className="block p-3">
+        <li className="border-black border-solid border-2 rounded-xl font-semibold hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out">
+          <a href="#" className="p-3 text-sm w-25 md:w-60 flex justify-center">
             The Office Quotes Quiz
           </a>
         </li>
-        <li className="border-black border-solid border-2 rounded-xl w-60 flex justify-center font-semibold hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out">
-          <a href="#" className="block p-3">
+        <li className="border-black border-solid border-2 rounded-xl font-semibold hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out">
+          <a href="" className="p-3 text-sm w-15 md:w-60 flex justify-center">
             Log in
           </a>
         </li>

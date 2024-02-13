13a394a392a52634a90382126b429fb6cd0c6233
Ola <128837261+aleksandrawrombel@users.noreply.github.com>
2024-02-13 12:29:43 +0100

    random quote author button implemented

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,22 +1,67 @@
 import React, { useState, useEffect } from 'react';
 import theOffice_logo from '../assets/theOffice_logo.png';
+import characters from '../assets/characters';
 
 const Quiz = () => {
   const [quote, setQuote] = useState([]);
+  const [incorrectOptions, setIncorrectOptions] = useState([]);
+  const [randomOption, setRandomOption] = useState([]);
 
   useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  //fetch a random quote, max 200 characters long
+
+  const fetchQuote = () => {
     fetch('https://officeapi.akashrajpurohit.com/quote/random')
       .then((response) => {
         return response.json();
       })
       .then((response) => {
         console.log(response);
-        setQuote(response);
+        if (response && response.quote.length <= 200) {
+          setQuote(response);
+          createIncorrectOptions(response.character);
+        } else {
+          fetchQuote();
+        }
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  };
+
+  const createIncorrectOptions = (correctAuthor) => {
+    const incorrectOptions = [];
+    const charactersCopy = [...characters];
+
+    //remove the correcy author from copied characters array
+    charactersCopy.splice(
+      charactersCopy.findIndex((character) => character.name === correctAuthor),
+      1
+    );
+
+    for (let i = 0; i < 3; i++) {
+      incorrectOptions.push(charactersCopy[i].name);
+    }
+
+    setIncorrectOptions(incorrectOptions);
+    randomize(correctAuthor, incorrectOptions);
+  };
+
+  //randomize answers
+
+  const randomize = (correctAuthor, incorrectOptions) => {
+    const options = [correctAuthor, ...incorrectOptions];
+
+    // Fisher-Yates shuffle algorithm
+    for (let i = options.length - 1; i > 0; i--) {
+      const randomIndex = Math.floor(Math.random() * (i + 1));
+      [options[i], (options[randomIndex] = options[randomIndex]), options[i]];
+    }
+    setRandomOption(options);
+  };
 
   return (
     <>
@@ -24,32 +69,26 @@ const Quiz = () => {
         <h1 className="text-white text-[1rem] md:text-[1.5rem] leading-relaxed">{quote.quote}</h1>
       </div>
       <section className="flex mb-1">
-        <button
-          type="submit"
-          className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out m-3"
-        >
-          {quote.character}
-        </button>
-        <button
-          type="submit"
-          className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out m-3"
-        >
-          AUTHOR
-        </button>
+        {randomOption.slice(0, 2).map((button, index) => (
+          <button
+            key={index}
+            type="submit"
+            className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out m-3"
+          >
+            {button}
+          </button>
+        ))}
       </section>
       <section className="flex mb-1">
-        <button
-          type="submit"
-          className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out m-3"
-        >
-          AUTHOR
-        </button>
-        <button
-          type="submit"
-          className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out m-3"
-        >
-          AUTHOR
-        </button>
+        {randomOption.slice(2, 4).map((button, index) => (
+          <button
+            key={index}
+            type="submit"
+            className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out m-3"
+          >
+            {button}
+          </button>
+        ))}
       </section>
     </>
   );

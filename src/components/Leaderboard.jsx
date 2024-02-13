9a4ae574fd0a8205e3f84c13ae386b6270a28b52
Ola <128837261+aleksandrawrombel@users.noreply.github.com>
2024-02-13 16:35:07 +0100

    leaderboard component created, conditional rendering added

---
+++ src/components/Leaderboard.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from 'react';
+
+const Leaderboard = () => {
+    return (
+        <div>Score!</div>
+    )
+};
+
+export default Leaderboard;
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import theOffice_logo from '../assets/theOffice_logo.png';
 import characters from '../assets/characters';
+import Leaderboard from './Leaderboard';
 
 const Quiz = ({ time }) => {
   const [quote, setQuote] = useState([]);
@@ -9,6 +10,7 @@ const Quiz = ({ time }) => {
   const [score, setScore] = useState(0);
   const [disabledButtons, setDisabledButtons] = useState(false);
   const [timer, setTimer] = useState(time);
+  const [quizFinished, isQuizFinished] = useState(false);
 
   // FETCHING THE QUOTE
   // timer
@@ -90,6 +92,9 @@ const Quiz = ({ time }) => {
     const interval = setInterval(() => {
       if (timer > 0) {
         setTimer((prev) => prev - 1);
+      } else {
+        clearInterval(interval);
+        isQuizFinished(true);
       }
     }, 1000);
     return () => {
@@ -99,49 +104,53 @@ const Quiz = ({ time }) => {
 
   //RENDER
 
-  return (
-    <>
-      <div className="flex flex-col justify-center items-center">
-        <div className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 m-3 text-center self-end">
-          Score: {score}
+  if (quizFinished) {
+    return <Leaderboard />;
+  } else {
+    return (
+      <>
+        <div className="flex flex-col justify-center items-center">
+          <div className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 m-3 text-center self-end">
+            Score: {score}
+          </div>
+          <div className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 m-3 text-center mb-6 self-end">
+            {timer}
+          </div>
+          <div className="border-white border-solid border-2 rounded-xl font-semibold w-[25rem] md:w-[40rem] h-[15rem] md:h-[15rem] bg-black flex justify-center items-center p-4 mb-5 overflow-hidden">
+            <h1 className="text-white text-[1rem] md:text-[1.5rem] leading-relaxed">{quote.quote}</h1>
+          </div>
         </div>
-        <div className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 m-3 text-center mb-6 self-end">
-          {timer}
-        </div>
-        <div className="border-white border-solid border-2 rounded-xl font-semibold w-[25rem] md:w-[40rem] h-[15rem] md:h-[15rem] bg-black flex justify-center items-center p-4 mb-5 overflow-hidden">
-          <h1 className="text-white text-[1rem] md:text-[1.5rem] leading-relaxed">{quote.quote}</h1>
-        </div>
-      </div>
-      <section className="flex mb-1">
-        {randomOption.slice(0, 2).map((button, index) => (
-          <button
-            key={index}
-            type="submit"
-            className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 md:w-60 hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out m-3 cursor-pointer"
-            onClick={() => {
-              handleAnswer(button);
-            }}
-            disabled={disabledButtons || (button !== quote.character ? true : false)}
-          >
-            {button}
-          </button>
-        ))}
-      </section>
-      <section className="flex mb-1">
-        {randomOption.slice(2, 4).map((button, index) => (
-          <button
-            key={index}
-            type="submit"
-            className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 md:w-60 hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out m-3 cursor-pointer"
-            onClick={() => handleAnswer(button)}
-            disabled={disabledButtons || (button !== quote.character ? true : false)}
-          >
-            {button}
-          </button>
-        ))}
-      </section>
-    </>
-  );
+        <section className="flex mb-1">
+          {randomOption.slice(0, 2).map((button, index) => (
+            <button
+              key={index}
+              type="submit"
+              className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 md:w-60 hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out m-3 cursor-pointer"
+              onClick={() => {
+                handleAnswer(button);
+              }}
+              disabled={disabledButtons || (button !== quote.character ? true : false)}
+            >
+              {button}
+            </button>
+          ))}
+        </section>
+        <section className="flex mb-1">
+          {randomOption.slice(2, 4).map((button, index) => (
+            <button
+              key={index}
+              type="submit"
+              className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-40 md:w-60 hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out m-3 cursor-pointer"
+              onClick={() => handleAnswer(button)}
+              disabled={disabledButtons || (button !== quote.character ? true : false)}
+            >
+              {button}
+            </button>
+          ))}
+        </section>
+      </>
+    );
+  }
 };
 
 export default Quiz;

ecd9f2674883cdcfd46de92b4859c0cd0e245627
Ola <128837261+aleksandrawrombel@users.noreply.github.com>
2024-02-14 15:34:11 +0100

    displaying leaderboard data from supabase done, RLS disabled for now

--- src/components/Leaderboard.jsx
+++ src/components/Leaderboard.jsx
@@ -10,7 +10,7 @@ const Leaderboard = ({ name, score }) => {
 
   async function insertData(name, score) {
     try {
-      const { data, error } = await supabase.from('Leaderboard').insert([{ user_id: name, score: score }]);
+      const { data, error } = await supabase.from('Leaderboard').insert([{ username: name, score: score }]);
       if (error) {
         throw error;
       }
@@ -20,8 +20,29 @@ const Leaderboard = ({ name, score }) => {
     }
   }
 
+  // GET DATA FROM SUPABASE AND RENDER 10 BEST SCORES
+
+  const [leaderboardData, setLeaderboardData] = useState([]);
+
+  async function getData() {
+    try {
+      const { data, error } = await supabase
+        .from('Leaderboard')
+        .select('*')
+        .order('score', { ascending: false })
+        .limit(10);
+      if (error) {
+        throw error;
+      }
+      setLeaderboardData(data);
+    } catch (error) {
+      console.log('error', error.message);
+    }
+  }
+
   useEffect(() => {
     insertData(name, score);
+    getData();
   }, [name, score]);
 
   return (
@@ -31,7 +52,12 @@ const Leaderboard = ({ name, score }) => {
         <p className="text-white text-[1rem] md:text-[1.2rem] leading-relaxed">{`You scored ${score} points!`}</p>
       </div>
       <div className="border-white border-solid border-2 rounded-xl font-semibold w-[20rem] md:w-[40rem] h-[30rem] md:h-[30rem] bg-black flex justify-center items-center p-4 mb-5 overflow-hidden">
-        <p className="text-white text-[1rem] md:text-[1.5rem] leading-relaxed">Leaderboard</p>
+        <ul>
+            {leaderboardData.map((user) => {
+                return <li key={user.id} className="text-white text-[1rem] md:text-[1.5rem] leading-relaxed">{user.username}: {user.score}</li>
+            })}
+        </ul>
+        {/* <p className="text-white text-[1rem] md:text-[1.5rem] leading-relaxed">Leaderboard</p> */}
       </div>
       <div className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold text-l w-[20rem] md:w-[40rem] h-[5rem] md:h-[5rem] flex justify-center items-center p-4 mb-5 overflow-hidden cursor-pointer animate-pulse">
         <button className="text-black text-[1rem] md:text-[1.25rem] leading-relaxed">Sign up</button>

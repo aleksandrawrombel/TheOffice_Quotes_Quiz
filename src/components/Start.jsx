ad226c4fca56974a91962865e67cac8c74327d42
Ola <128837261+aleksandrawrombel@users.noreply.github.com>
2024-02-15 11:24:42 +0100

    username input validation added

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -4,7 +4,10 @@ import Loading from './Loading';
 import Quiz from './Quiz';
 
 const Start = ({ setName }) => {
+  // username and validation state
   const [inputName, setInputName] = useState('');
+  const [error, setError] = useState('');
+  // loading page and quiz start state
   const [loading, setLoading] = useState(false);
   const [gameStarted, setGameStarted] = useState(false);
 
@@ -18,8 +21,15 @@ const Start = ({ setName }) => {
     }
   }, [loading]);
 
+  const validUsernameRegex = /^[a-zA-Z0-9]{3,20}$/;
+
   const handleGameStart = (e) => {
     e.preventDefault();
+
+    if (!validUsernameRegex.test(inputName)) {
+      setError('Username must be between 3 and 20 characters long, no special characters allowed.');
+      return;
+    }
     setLoading(true);
   };
 
@@ -31,9 +41,9 @@ const Start = ({ setName }) => {
             <h1 className="text-white text-[1.75rem] md:text-[3rem]">The Office Quotes Quiz</h1>
             <img src={theOffice_logo} alt="The Office logo" className="w-48 md:w-52 h-48 md:h-52" />
           </div>
-          <form className="flex flex-col justify-center items-center h-[10rem]" onSubmit={handleGameStart}>
+          <form className="flex flex-col justify-center items-center h-[13rem]" noValidate>
             <input
-              className="p-3 m-5 bg-office_gray border-black border-solid border-2 rounded-xl font-semibold text-l w-60 md:flex justify-center text-center focus:outline-none"
+              className="p-3 m-5 mt-11 bg-office_gray border-black border-solid border-2 rounded-xl font-semibold text-l w-60 md:flex justify-center text-center focus:outline-none"
               placeholder="Enter your name"
               value={inputName}
               onChange={(e) => setInputName(e.target.value)}
@@ -42,11 +52,17 @@ const Start = ({ setName }) => {
               <button
                 type="submit"
                 className="bg-office_gray border-black border-solid border-2 rounded-xl font-semibold p-3 text-l w-60 md:flex justify-center hover:bg-office_button hover:shadow-lg transition duration-300 ease-in-out animate-pulse"
+                onClick={handleGameStart}
               >
                 Start
               </button>
             )}
           </form>
+          {error && (
+            <span className="text-[0.8rem] p-1 mb-5 text-red-800 w-[14rem] text-center border-red-800 border-dashed border-2 rounded-xl font-semibold">
+              {error}
+            </span>
+          )}
         </>
       )}
       {loading && <Loading />}

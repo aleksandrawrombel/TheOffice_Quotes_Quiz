8255f66caf930b04606d1ce8c1dbed113345479b
Ola <128837261+aleksandrawrombel@users.noreply.github.com>
2024-02-16 14:22:05 +0100

    registration component styling

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -7,13 +7,33 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 // REGISTER
 
-// const [email, setEmail] = useState("");
-// const [password, setPassword] = useState("");
+// const [email, setEmail] = useState('');
+// const [password, setPassword] = useState('');
+// const [username, setUsername] = useState('');
 
 const Register = () => {
   return (
     <>
-      <p>Registration</p>
+      <div className="flex flex-col justify-center items-center">
+        <div className="border-white border-solid border-2 rounded-xl font-semibold w-[20rem] md:w-[40rem] h-[35rem] md:h-[35rem] flex justify-center items-center flex-col p-4 mb-5 overflow-hidden blackboard font-office_noto">
+          {/* <form>
+            <input type="email" value={email} placeholder="Email" onChange={(e) => setEmail(e.target.value)}></input>
+            <input
+              type="password"
+              value={password}
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+            <input
+              type="text"
+              value={username}
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+            ></input>
+            <button>Register</button>
+          </form> */}
+        </div>
+      </div>
     </>
   );
 };
